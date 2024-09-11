6bbff679119428296b3aed26a249bbe344f52898
johnpooshids <johnpooshids@colonblow.io>
2024-09-11 14:31:19 -0700

    test orgid

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "option" {
   type        = number
   description = "Set this to 1 (PrincipalOrgID), 2 (PrincipalAccount), 3 (<service>:*:<local.account>:*), 4 (OU-Path) or 5 (Deny All)"
-  default     = 5 
+  default     = 1 
 }
 
 locals {
