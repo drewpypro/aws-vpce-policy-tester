95ad573f498aa86ab1a756d55761707ff65030c1
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-11-14 18:47:50 -0700

    updating local test

--- module/variables.tf
+++ module/variables.tf
@@ -1,12 +1,21 @@
 variable "services" {
   type        = list(string)
   description = "List of AWS services for VPC endpoints"
-  
+
   validation {
-    condition     = can([regex("^(autoscaling|dms|ec2|ec2messages|elasticloadbalancing|logs|monitoring|rds|secretsmanager|sns|sqs|ssm|ssmmessages|sts)$", "test")])
-    error_message = "Invalid service. Must be one of: autoscaling, dms, ec2, ec2messages, elasticloadbalancing, logs, monitoring, rds, secretsmanager, sns, sqs, ssm, ssmmessages, sts"
+    condition     = alltrue([for service in var.services : contains(local.authorized_services, service)])
+    error_message = "Invalid service(s) provided. Allowed values: $(join(", ", local.authorized_services)}"
   }
 }
+
+locals {
+    authorized_services = [
+        "autoscaling", "dms", "ec2", "ec2messages", "elasticloadbalancing", 
+        "logs", "monitoring", "rds", "secretsmanager", 
+        "sns", "sqs", "ssm", "ssmmessages", "sts"
+    ]
+}
+
 
 variable "subnet_cidrs" {
   default = ["192.168.0.0/16"]
