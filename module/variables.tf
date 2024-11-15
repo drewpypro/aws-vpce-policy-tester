527cff5b3c4923b13f032c94d211cbc6c24c8c55
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-11-14 19:25:19 -0700

    bad tfstate, trying again

--- module/variables.tf
+++ module/variables.tf
@@ -1,24 +1,24 @@
 variable "services" {
-  type        = list(string)
   description = "List of AWS services for VPC endpoints"
+  type        = list(string)
 
-#   validation {
-#     condition     = alltrue([for service in var.services : contains(local.authorized_services, service)])
-#     error_message = "Invalid service(s) provided. Allowed values: ${join(",", local.authorized_services)}"
-#   }
+  validation {
+    condition     = alltrue([for service in var.services : service in [
+      "autoscaling", "dms", "ec2", "ec2messages",
+      "elasticloadbalancing", "logs", "monitoring", "rds",
+      "secretsmanager", "sns", "sqs", "ssm",
+      "ssmmessages", "sts"
+    ]])
+    error_message = "Invalid service(s) provided. Allowed values: autoscaling, dms, ec2, ec2messages, elasticloadbalancing, logs, monitoring, rds, secretsmanager, sns, sqs, ssm, ssmmessages, sts."
+  }
 }
-
-# locals {
-#     authorized_services = [
-#         "autoscaling", "dms", "ec2", "ec2messages", "elasticloadbalancing", 
-#         "logs", "monitoring", "rds", "secretsmanager", 
-#         "sns", "sqs", "ssm", "ssmmessages", "sts"
-#     ]
-# }
 
 
 variable "subnet_cidrs" {
-  default = ["192.168.0.0/16"]
+  validation {
+    condition     = alltrue([for cidr in var.subnet_cidrs : length(regex("[0-9]+$", cidr)) > 0 && tonumber(regex("[0-9]+$", cidr)) >= 25])
+    error_message = "Subnet is big doo doo (must be /25 or less)"
+  }
 }
 
 variable "vpc_id" {
