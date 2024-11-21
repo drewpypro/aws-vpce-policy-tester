c0dfb5d4945583e4d83c00ab48a4702b2b075c7e
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-11-20 22:44:33 -0700

    update outputs

--- modulev2/outputs.tf
+++ modulev2/outputs.tf
@@ -1,4 +1,4 @@
-output "security_group_ids" {
+output "security_group_ids_modulev2" {
   description = "Map of security group IDs keyed by service name"
   value = { for sg in aws_security_group.this : sg.tags["Service"] => sg.id }
 }
